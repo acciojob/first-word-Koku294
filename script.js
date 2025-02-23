a98fde2a964977003c9f47f078fa9ecf0b0841c6
Koku294 <90713556+Koku294@users.noreply.github.com>
2025-02-23 22:41:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+  // Trim the input to remove leading or trailing spaces
+  str = str.trim();
+  
+  // Check if the string contains any space
+  const spaceIndex = str.indexOf(' ');
+
+  // If space exists, return the substring before the first space
+  if (spaceIndex !== -1) {
+    return str.substring(0, spaceIndex);
+  }
+
+  // If no space, return the entire string
+  return str;
 }
 
 // Do not change the code below
